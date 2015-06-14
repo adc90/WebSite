55cdb057a9f8cfab833fccd764b84e791dc04ec2
adc90 <aaronc81590@gmail.com>
2015-06-14 14:01:18 -0400

    Changed directory structure

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
